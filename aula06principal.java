94b8fe110bdd84e97899bdf9f1cb7bf271d08802
devrobertocsf <robertocarlosw5251@gmail.com>
2026-05-05 19:38:26 -0300

    código

---
+++ aula06principal.java
@@ -0,0 +1,16 @@
+package aula06;
+
+public class aula06principal {
+
+	public static void main(String[] args) {
+		ControleRemoto cr1 = new ControleRemoto();
+		cr1.ligar();
+		cr1.maisVolume();
+		cr1.play();
+	//	cr1.ligarMudo();
+		cr1.abrirMenu();
+	
+
+	}
+
+}
